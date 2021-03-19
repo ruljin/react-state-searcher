4b117ae768c56f226aaf5de65dfae917f7bd1ce2
fkuca <fkuca91@gmail.com>
2021-03-19 19:49:49 +0100

    Add list filtering

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Fuse from "fuse.js";
+import states from "../states.json";
+
+const options = { keys: ["state", "code"] };
+const fuse = new Fuse(states, options);
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [resultsList, setResultsList] = useState([]);
+
+  useEffect(() => {
+    setResultsList(fuse.search(query));
+  }, [query]);
 
   return (
     <div className="dropdown is-active">
@@ -16,8 +26,12 @@ const App = () => {
       </div>
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
-          <a className="dropdown-item">Dropdown item</a>
-          <a className="dropdown-item s-active">Other dropdown item</a>
+          {resultsList.map(({ item: { state, code } }, index) => (
+            <a key={code} className="dropdown-item" tabIndex={index + 1}>
+              {state}
+              {/* is-active */}
+            </a>
+          ))}
         </div>
       </div>
     </div>
