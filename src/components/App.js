15547b9a5079ab4d647cc4f47a9644a1859425fe
fkuca <fkuca91@gmail.com>
2021-03-19 20:09:58 +0100

    Add moving on list with keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Fuse from "fuse.js";
 import states from "../states.json";
+import classnames from "classnames";
 
 const options = { keys: ["state", "code"] };
 const fuse = new Fuse(states, options);
@@ -8,9 +9,27 @@ const fuse = new Fuse(states, options);
 const App = () => {
   const [query, setQuery] = useState("");
   const [resultsList, setResultsList] = useState([]);
+  const [selected, setSelected] = useState(0);
+
+  const onSelect = (event) => {
+    const { key } = event;
+    if (key === "ArrowUp") {
+      const newSelected = selected - 1;
+      if (newSelected < 0) {
+        return;
+      }
+      setSelected(newSelected);
+    } else if (key === "ArrowDown") {
+      const newSelected = selected + 1;
+      if (newSelected > resultsList.length - 1) {
+        return;
+      }
+      setSelected(newSelected);
+    }
+  };
 
   useEffect(() => {
-    setResultsList(fuse.search(query));
+    setResultsList(fuse.search(query).slice(0, 6));
   }, [query]);
 
   return (
@@ -22,14 +41,20 @@ const App = () => {
           placeholder="Search..."
           value={query}
           onChange={({ target }) => setQuery(target.value)}
+          onKeyUp={onSelect}
         />
       </div>
       <div className="dropdown-menu" id="dropdown-menu" role="menu">
         <div className="dropdown-content">
           {resultsList.map(({ item: { state, code } }, index) => (
-            <a key={code} className="dropdown-item" tabIndex={index + 1}>
+            <a
+              key={code}
+              className={classnames("dropdown-item", {
+                "is-active": selected === index,
+              })}
+              onMouseEnter={() => setSelected(index)}
+            >
               {state}
-              {/* is-active */}
             </a>
           ))}
         </div>
